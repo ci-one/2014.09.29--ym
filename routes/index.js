e83f1d105434c55c154ac8bafa74af8122dbdcfe
ci-one <seongyu_kim@ci-one.com>
2014-09-30 10:18:09 +0900

    라우트 수정

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
+
+router.get('/less', function(req, res) {
+    res.render('less', { title: 'less' });
+});
+
+router.get('/machine', function(req, res) {
+    res.render('machine', { title: 'machine' });
+});
+
 
 module.exports = router;
