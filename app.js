e47c3a4e4029c35c410846ed42c79b5c9d005d24
Sim Jeongmee <sinjeongmee@ci-one.com>
2014-10-01 15:53:05 +0900

    lll

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes2 = require('./routes/index2');
+var routes3 = require('./routes/index3');
 
 var app = express();
 
