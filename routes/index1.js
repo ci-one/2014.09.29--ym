b47612e4b7a180b43a7811d207f63edfa24d59b4
ci-one <seongyu_kim@ci-one.com>
2014-10-01 15:39:01 +0900

    2014-10-01    15:38 강문식

--- routes/index1.js
+++ routes/index1.js
@@ -11,13 +11,55 @@ router1.get('/intro1', function(req, res) {
     res.render('machine/machine_jade/043-1_S_co_intro1_machine', { title: 'machine' });
 });
 
-router1.get('/product_List1', function(req, res) {
+router1.get('/product_List11', function(req, res) {
     res.render('machine/machine_jade/043-1_S_product_List1_machine', { title: 'machine' });
 });
 
-router1.get('/product_List2', function(req, res) {
-    res.render('machine/machine_jade/043-1_S_product_List2', { title: 'machine' });
+router1.get('/product_List12', function(req, res) {
+    res.render('machine/machine_jade/043-1_S_product_List2_machine', { title: 'machine' });
 });
+
+router1.get('/customer_Write11', function(req, res) {
+    res.render('machine/machine_jade/043-1_S_customer_Write1_machine', { title: 'machine' });
+});
+
+router1.get('/directions11', function(req, res) {
+    res.render('machine/machine_jade/043-1_S_co_directions1_machine', { title: 'machine' });
+});
+
+router1.get('/product1_View11', function(req, res) {
+    res.render('machine/machine_jade/043-1_S_product1_View1_machine', { title: 'machine' });
+});
+
+router1.get('/customer_write11', function(req, res) {
+    res.render('machine/machine_jade/043-1_S_customer_Write1_machine', { title: 'machine' });
+});
+
+
+router1.get('/customer_write13', function(req, res) {
+    res.render('machine/machine_jade/043-1_S_customer_Write3_machine', { title: 'machine' });
+});
+
+
+router1.get('/notice_List11', function(req, res) {
+    res.render('machine/machine_jade/043-1_S_notice_List1_machine', { title: 'machine' });
+});
+
+
+router1.get('/product_Write11', function(req, res) {
+    res.render('machine/machine_jade/043-1_S_product_Write1_machine', { title: 'machine' });
+});
+
+
+router1.get('/product_View12', function(req, res) {
+    res.render('machine/machine_jade/043-1_S_product_View2_machine', { title: 'machine' });
+});
+
+
+router1.get('/product_CategoryMod12', function(req, res) {
+    res.render('machine/machine_jade/043-1_S_product_CategoryMod2_machine', { title: 'machine' });
+});
+
 
 
 router1.get('/test', function(req, res) {
